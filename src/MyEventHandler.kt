63cf169de32819a7393c1c3b547b75de63bf9dab
whenke <wyatt.henke@gmail.com>
2017-12-03 02:30:09 -0500

    made coords private in MyEventHandler

--- src/MyEventHandler.kt
+++ src/MyEventHandler.kt
@@ -2,7 +2,7 @@ import javafx.event.EventHandler
 import javafx.scene.input.MouseEvent
 import java.util.*
 
-class MyEventHandler(val coords: LinkedList<Coordinate>) : EventHandler<MouseEvent> {
+class MyEventHandler(private val coords: LinkedList<Coordinate>) : EventHandler<MouseEvent> {
     override fun handle(event: MouseEvent) {
         coords.add(Coordinate(event.x, event.y))
         event.consume()
